6ea7031811c3691af553446ddf906378e0bab2a8
TameoCasta <mateocs890@gmail.com>
2021-12-11 09:59:21 -0500

    app

---
+++ app/src/main/java/com/example/appmarketplacenew/RegisterActivity.java
@@ -0,0 +1,6 @@
+package com.example.appmarketplacenew;
+
+import android.app.Activity;
+
+public class RegisterActivity extends Activity {
+}
--- app/src/main/java/com/example/appmarketplacenew/authActivity.kt
+++ app/src/main/java/com/example/appmarketplacenew/authActivity.kt
@@ -2,34 +2,23 @@ package com.example.appmarketplacenew
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.content.DialogInterface
-import android.content.Intent
-import android.widget.EditText
-import android.widget.Switch
-import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
-import com.google.android.material.textfield.TextInputLayout
-import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
-import java.util.regex.Pattern
+import kotlin.concurrent.thread
 
 
 class authActivity : AppCompatActivity() {
-    private var signUpButton: EditText? = null
-    private var emailEditText: EditText? = null
-    private var passwordEditText: EditText? = null
+
 
     var db = FirebaseFirestore.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
+        Thread.sleep(2000)
+        setTheme(R.style.SplashTheme)
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
 
-        signUpButton= findViewById(R.id.signUpButton);
-        emailEditText= findViewById(R.id.emailEditText);
-        passwordEditText= findViewById(R.id.passwordEditText);
+
 
 
         //setup
@@ -40,9 +29,6 @@ class authActivity : AppCompatActivity() {
     private fun setup() {
         title = "Autenticacion"
 
-        signUpButton.setOnClickListener {
-            if (emailEditText.text.isNotEmpty()&& passwordEditText.text.isNotEmpty()){
-        }
 
 
 
